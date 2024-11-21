2ae7820da0c21fcfe279894f67f388209b773752
Black Sheep <barbarblacksheep03@gmail.com>
2024-11-20 18:49:59 -0800

    deploy on github attempt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from 'astro/config';
 // Integrations
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import github from '@astrojs/github';
 
 // Astro Configuration
 export default defineConfig({
+  site: 'https://blacksheepshaun.github.io/neonPortfoli/', // replace with your GitHub Pages URL
+  outDir: 'dist', // default build output
+  base: '/neonPortfoli/', // add this if the project is not deployed on your root domain
   integrations: [
+    github(),
     tailwind(), // Tailwind CSS integration
     react(),    // React integration
   ],

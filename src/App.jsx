fb5a57e151bb31606ed76d5e3424cc1d7d80d047
Black Sheep <barbarblacksheep03@gmail.com>
2024-11-20 17:05:15 -0800

    deployment dynamic error

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ContactUs from './components/contactForm.jsx'; // Corrected path
-import './App.css';
+// import React from 'react';
+// import ContactUs from './components/contactForm.jsx'; // Corrected path
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Contact Us</h1>
-      <ContactUs />
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Contact Us</h1>
+//       <ContactUs />
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
